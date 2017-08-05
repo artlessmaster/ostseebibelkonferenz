5235a68bb180634ae5c0ab811d8e6e45a35cf5f1
Danny Ruß <artless.characters@gmail.com>
2017-08-05 15:54:29 +0200

    Anmeldeformular

--- php/absenden.php
+++ php/absenden.php
@@ -6,4 +6,19 @@ $email = $_POST['email'];
 $anzahlErwachsene = $_POST['anzahlErwachsene'];
 $anzahlKinder = $_POST['anzahlKinder'];
 
+$empfaenger = "kontakt@ostseebibelkonferenz.de";
+$absendername = "Anmeldeformular";
+$absenderemail = $email;
+$betreff = "Neue Anmeldung";
+$from = "From: Nils Reimers <absender@domain.de>";
+$text = "Es ist eine neue Anmeldung über das Anmeldeformualr eingetroffen. Folgende Daten wurden übermittelt:
+
+Name, Vorname ".$nachname.", ".$vorname."
+Email: ".$email."
+Anzahl Erwachsene: ".$anzahlErwachsene."
+Anzahl Kinder: ".$anzahlKinder;
+ 
+mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
+
+echo('Vielen Dank! Wir freuen uns über Ihre Anmeldung!')
 ?>
