b4fa5c15461df2c2dd6c83feda66c2cb64cc7c8e
Danny Ruß <artless.characters@gmail.com>
2017-08-05 17:45:29 +0200

    funktionierendes Anmeldeformular

--- php/absenden.php
+++ php/absenden.php
@@ -5,7 +5,7 @@ $nachname = $_POST['nachname'];
 $email = $_POST['email'];
 $anzahlErwachsene = $_POST['anzahlErwachsene'];
 $anzahlKinder = $_POST['anzahlKinder'];
-
+    
 $empfaenger = "kontakt@ostseebibelkonferenz.de";
 $absendername = "Anmeldeformular";
 $absenderemail = $email;
@@ -13,11 +13,12 @@ $betreff = "Neue Anmeldung zur OBK von ".$vorname." ".$nachname;
 $from = "From: Nils Reimers <absender@domain.de>";
 $text = "Es ist eine neue Anmeldung über das Anmeldeformualr eingetroffen. Folgende Daten wurden übermittelt:
 
-Name, Vorname ".$nachname.", ".$vorname."
-Email: ".$email."
-Anzahl Erwachsene: ".$anzahlErwachsene."
-Anzahl Kinder: ".$anzahlKinder;
- 
+    Name: ".$nachname."
+    Vorname: ".$vorname."
+    Email: ".$email."
+    Anzahl Erwachsene: ".$anzahlErwachsene."
+    Anzahl Kinder: ".$anzahlKinder;
+
 mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
 
 echo('Vielen Dank! Wir freuen uns über Ihre Anmeldung!')
--- php/senden.php
+++ php/senden.php
@@ -2,34 +2,33 @@
  // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $vorname = strip_tags(trim($_POST["vorname"]));
-				$vorname = str_replace(array("\r","\n"),array(" "," "),$vorname);
-        $nachname = strip_tags(trim($_POST["nachname"]));
-				$nachname = str_replace(array("\r","\n"),array(" "," "),$nachname);
-        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
-        $anzahlErwachsene = trim($_POST["anzahlErwachsene"]);
-        $anzahlKinder = trim($_POST["anzahlKinder"]);
-
+        $vorname = $_POST['vorname'];
+        $nachname = $_POST['nachname'];
+        $email = $_POST['email'];
+        $anzahlErwachsene = $_POST['anzahlErwachsene'];
+        $anzahlKinder = $_POST['anzahlKinder'];
+        
         // Check that data was sent to the mailer.
-        if ( empty($vorname) OR empty($nachname) OR empty($anzahlErwachsene)OR empty($anzahlKinder) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ($vorname == '' || $nachname == '' || $anzahlErwachsene == '' || $anzahlKinder == '' || $email == '') {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Hoppla! Es gab ein Problem mit deiner Eingabe. Bitte versuche es erneut.";
-            exit;
+            echo('Hoppla! Es gab ein Problem mit deiner Eingabe. Bitte versuche es erneut.')
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
         $recipient = "kontakt@ostseebibelkonferenz.de";
 
         // Set the email subject.
         $subject = "Neue OBK Anmeldung von $vorname $nachname";
 
         // Build the email content.
-        $email_content = "Name: $vorname $nachname\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Anzahl Erwachsene: $anzahlErwachsene\n";
-        $email_content .= "Anzahl Kinder: $anzahlKinder\n";
+        $email_content = "Es ist eine neue Anmeldung über das Anmeldeformualr eingetroffen. Folgende Daten wurden übermittelt:
+
+            Name: ".$nachname."
+            Vorname: ".$vorname."
+            Email: ".$email."
+            Anzahl Erwachsene: ".$anzahlErwachsene."
+            Anzahl Kinder: ".$anzahlKinder;
 
         // Build the email headers.
         $email_headers = "From: $vorname $nachname <$email>";
@@ -38,17 +37,17 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Vielen Dank! Deine Anmeldung wurde abgeschickt.";
+            echo('Vielen Dank! Deine Anmeldung wurde abgeschickt.')
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Hoppla! Irgendetwas lief schief, wir konnten deine Anmeldung nicht empfangen.";
+            echo('Hoppla! Irgendetwas lief schief, wir konnten deine Anmeldung nicht empfangen.')
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "Es gab ein Problem mit deiner Eingabe. Bitte versuche es erneut.";
+        echo('Es gab ein Problem mit deiner Eingabe. Bitte versuche es erneut.')
     }
 
 ?>
