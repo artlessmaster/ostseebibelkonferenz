2c54152dbe2d8dc337162aaaa7d9d1196be6a558
Danny Ruß <artless.characters@gmail.com>
2017-08-11 01:27:10 +0200

    Anmeldebestätigung an den Empfänger

--- php/absenden.php
+++ php/absenden.php
@@ -7,8 +7,8 @@ $anzahlErwachsene = $_POST['anzahlErwachsene'];
 $anzahlKleinkinder = $_POST['anzahlKleinkinder'];
 $anzahlKinder = $_POST['anzahlKinder'];
     
-$empfaenger = "danny.russ@cv-kiel.de";
-$absendername = "Anmeldeformular";
+$empfaenger = "kontakt@ostseebibelkonferenz.de";
+$absendername = "Anmeldeformular OBK";
 $absenderemail = $email;
 $betreff = "Neue Anmeldung zur OBK von ".$vorname." ".$nachname;
 $from = "From: $absendername <$absendermail>";
@@ -21,7 +21,29 @@ $text = "Es ist eine neue Anmeldung über das Anmeldeformualr eingetroffen. Folg
     Anzahl Kleinkinder: ".$anzahlKleinkinder."
     Anzahl Kinder: ".$anzahlKinder;
 
+//Anmeldung
 mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");
 
+//Funktionsbestätigung für Absender
+
+$textAbsender = "Lieber ".$vorname.",
+
+Deine Anmeldung wurde abgeschickt. Folgendes hast Du uns angegeben:
+
+    Name: ".$nachname."
+    Vorname: ".$vorname."
+    Email: ".$email."
+    Anzahl Erwachsene: ".$anzahlErwachsene."
+    Anzahl Kleinkinder: ".$anzahlKleinkinder."
+    Anzahl Kinder: ".$anzahlKinder."
+
+Schön, dass Du dabei bist!
+
+Viele Grüße
+Dein OBK-Team";
+
+mail($email, "Anmeldebestätigung zur OBK", $textAbsender, "From: $absendername <$empfaenger>");
+
 echo('Vielen Dank! Wir freuen uns über Deine Anmeldung!')
+
 ?>
